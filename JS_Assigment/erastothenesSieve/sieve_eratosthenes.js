0327cad371f45854bd523745ebf65e0a4f4fb2be
Omar Vidaña Rodríguez <107644289+OmVidana@users.noreply.github.com>
2022-09-27 02:11:59 -0500

    Sieve Eratosthenes Done!

--- JS_Assigment/erastothenesSieve/sieve_eratosthenes.js
+++ JS_Assigment/erastothenesSieve/sieve_eratosthenes.js
@@ -3,14 +3,21 @@
     to find all of the smaller primes (below 10 million or so).
 */
 
-var sieve = function (n) {
+var sieve = function () {
     "use strict";
-
+    var n = parseInt(document.getElementById('eratos').value);
     var array = [], primes = [], i, j;
-
-
-
-    return primes;
+    for (i = 2; i <= n; i++) {
+        if (array.indexOf(i) == -1) {
+            primes.push(i);
+            for (j = i*i; j <= n; j+=i) {
+                array.push(j)
+            }
+        }
+    }
+    var result = primes.join(', ')
+    document.getElementById('primes').innerHTML = result;
+    console.log(result);
+    return result;
 };
 
-console.log(sieve(1000000));
