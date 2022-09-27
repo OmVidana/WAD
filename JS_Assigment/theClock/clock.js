4decf752b890579b147b75ec226ec4ed24d83bdc
Omar Vidaña Rodríguez <107644289+OmVidana@users.noreply.github.com>
2022-09-27 01:40:34 -0500

    Clock Done!

--- JS_Assigment/theClock/clock.js
+++ JS_Assigment/theClock/clock.js
@@ -9,46 +9,49 @@ function drawFace(ctx, radius) {
     var grad;
     ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2*Math.PI);
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'black';
     ctx.fill();
 
     // Draw the edge circle with gradient
-    // TODO: (Optional) add a gradient circle
-
+    grad = ctx.createRadialGradient(0, 0, radius, 0, 0, 50);
+    grad.addColorStop(0.3, "gray");
     // Center circle
-    // TODO: make the central black circle
+    grad.addColorStop(1, "black");
+    ctx.fillStyle = grad;
+    ctx.fill() 
 }
 
 function drawNumbers(ctx, radius) {
-    //TODO: Make sure you show all the numbers
     var ang;
-    var num =1;
-    ctx.font = radius*0.15 + "px arial";
-    ctx.textBaseline="middle";
-    ctx.fillStyle = '#333';
-    ctx.textAlign="center";
-    ang = num * Math.PI / 6;
-    ctx.rotate(ang);
-    ctx.translate(0, -radius*0.85);
-    ctx.rotate(-ang);
-    ctx.fillText(num.toString(), 0, 0);
-    ctx.rotate(ang);
-    ctx.translate(0, radius*0.85);
-    ctx.rotate(-ang);
+    for(var num = 1; num <= 12; num++) {
+        ctx.font = radius*0.15 + "px arial";
+        ctx.textBaseline="middle";
+        ctx.fillStyle = '#eee';
+        ctx.textAlign="center";
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius*0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius*0.85);
+        ctx.rotate(-ang);
+    }
 }
 
 function drawTime(ctx, radius){
-    // TODO: Calculate the angles of every hand depending on the time
     var now = new Date();
     var hour = now.getHours();
     var minute = now.getMinutes();
     var second = now.getSeconds();
     //hour
-    hour=hour%12;
+    hour = (hour*Math.PI/6) + (minute*Math.PI/(6*60)) + (second*Math.PI/(360*60));
     drawHand(ctx, hour, radius*0.5, radius*0.07);
     //minute
+    minute = (minute*Math.PI/30) + (second*Math.PI/(30*60));
     drawHand(ctx, minute, radius*0.8, radius*0.07);
     // second
+    second = second*Math.PI/30;
     drawHand(ctx, second, radius*0.9, radius*0.02);
 }
 
@@ -56,6 +59,7 @@ function drawHand(ctx, pos, length, width) {
     ctx.beginPath();
     ctx.lineWidth = width;
     ctx.lineCap = "round";
+    ctx.strokeStyle = '#eee';
     ctx.moveTo(0,0);
     ctx.rotate(pos);
     ctx.lineTo(0, -length);
