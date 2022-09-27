f4bcf1e4869fc35a83739eb953593f4191d7b0ce
Omar Vidaña Rodríguez <107644289+OmVidana@users.noreply.github.com>
2022-09-26 23:19:54 -0500

    Prime Factors Done!
    It's printed in a Span and in console.log()

--- JS_Assigment/primeFactors/prime_factors.js
+++ JS_Assigment/primeFactors/prime_factors.js
@@ -2,28 +2,23 @@
     Prime Factorization - Have the user enter a number and find
     all Prime Factors (if there are any) and display them.
 */
-
-var getPrimeFactors = function (n) {
-    "use strict";
-
-    function isPrime(n) {
-        var i;
-
-        for (i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i === 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    var i, sequence = [];
-
-    //TODO: Check which numbers are factors of n and also check if
-    // that number also happens to be a prime
-
-    return sequence;
+var primeFactors = function() {
+    var inputValue = document.getElementById("numberCheck").value;
+    document.getElementById("factors").innerHTML = getPrimeFactors(inputValue).join(' ')
+    console.log(getPrimeFactors(inputValue).toString())
 };
 
+var getPrimeFactors = function(n) {
+    "use strict";
+    var sequence = [];
+    for(var i = 2; i <= n; i++) {
+        if(n % i === 0) {
+            sequence.push(i) 
+            n /= i
+        }
+    }   
+    return sequence;
+};
 // the prime factors for this number are: [ 2, 3, 5, 7, 11, 13 ]
-console.log(getPrimeFactors(30030));
+// console.log(getPrimeFactors(30030))
+
