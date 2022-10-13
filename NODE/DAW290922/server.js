7a2c392f536ee0b52d2f1c193017339ae62d6764
Omar Vidaña Rodríguez <107644289+OmVidana@users.noreply.github.com>
2022-10-13 13:04:14 -0500

    Oct 11 Last Class Activity
    yes

--- NODE/DAW290922/server.js
+++ NODE/DAW290922/server.js
@@ -3,26 +3,31 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static("public"));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
 
 app.get("/",(req, res) => {
-    res.sendFile(__dirname + "/public/html/index.html");
+    let name = req.query.name;
+    // res.sendFile(__dirname + "/public/html/index.html");
+    res.render('home.ejs', {name: name});
 });
 
 app.get("/students", (req, res) => {
     let name = req.query.name;
-    res.send("Hello " + name);
+    // res.send("Hello " + name);
+    res.render('test.ejs', {name: name});
 });
 
 app.post("/students", (req, res) => {
     let name = req.body.name;
-    res.send("Hello secure " + name);
+    res.send('Hello secure ' + name);
 });
 
 
-app.get("/teachers/:name", (req, res) => {
-    res.send("Hello Professor " + req.params.name);
+app.get('/teachers/:name', (req, res) => {
+    res.send('Hello Professor ' + req.params.name);
 });
 
 app.listen(3000,(err) => {
-    console.log("Listening on port 3000")
+    console.log('Listening on port 3000')
 });
